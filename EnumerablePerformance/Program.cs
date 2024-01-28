8bb3d85ab6337dd53005d51a4353dda308722070
Taylor Maxwell <taylor@imaxwell.net>
2024-01-28 07:21:19 -0800

    Add project files.

---
+++ EnumerablePerformance/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var customers = GetCustomers();
+var count = customers.Count();
+Console.WriteLine($"There are {count} customers");
+
+foreach(var customer in customers)
+{
+    Console.WriteLine($"{customer.FullName} is {customer.Age} years old");
+}
+
+IEnumerable<Customer> GetCustomers()
+{
+    var lines = File.ReadAllLines("./customers.csv");
+    foreach(var line in lines)
+    {
+        var parts = line.Split(',');
+        yield return new Customer(parts[0], int.Parse(parts[1]));
+    }
+}
+
+record Customer(string FullName, int Age);
